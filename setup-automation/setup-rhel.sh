6005aa7af0775327267ac9fc478ea10f1557d11b
Wilson Harris <wharris@redhat.com>
2025-06-13 16:27:55 -0400

    Update restore edited content

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,15 +5,11 @@
 echo "Just thinking about everything I have been through, and how huge it all feels." >> quote.txt
 echo "The fact that it is just a small part of something larger" >> quote.txt
 
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
-
-# install locate
-dnf install -y mlocate
-
 # update locate db
 updatedb
 
 # inject a message into messages
 logger -s "Hello World"
 
-rm -rf /root/*.repo
+systemctl start mlocate-updatedb.timer
+systemctl enable mlocate-updatedb.timer
