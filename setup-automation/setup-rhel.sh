74f84d2ba7c9811ef2a37f4e2beadf0b33785b4b
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:20 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -2,6 +2,10 @@
 
 # create notes file and populate with some text
 
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 touch /root/quote.txt
 echo "Just thinking about everything I have been through, and how huge it all feels." >> /root/quote.txt
 echo "The fact that it is just a small part of something larger" >> /root/quote.txt
